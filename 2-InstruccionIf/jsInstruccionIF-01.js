2f51307f915941d84b5fc4fce7d86c676155e421
Emaa0596 <emanueldiaz0596@gmail.com>
2023-02-07 10:08:19 -0300

    5 ejercicios if

--- 2-InstruccionIf/jsInstruccionIF-01.js
+++ 2-InstruccionIf/jsInstruccionIF-01.js
@@ -1,6 +1,36 @@
+
+/*
+	if(condicion)//verdadero o falso (0) true -> verdadero false -> falso
+	{
+		codigo...;
+	}else
+	{
+		codigo...;		
+	}
+
+	operadores aritmeticos:
+	+ - * / %
+	operadores relacionales o condicionales
+	< > <= >= == !=
+	operadores logicos
+    condicionUno	&&  condicionDos            ||               !
+	y logica                                o logica         negacion
+	*/
+
+
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
+	let edad;
+
+	edad = parseInt (document.getElementById("txtIdEdad").value);
+	
+	if(edad == 15){
+		alert("Niña bonita"); 
+
+	}
+	else 
+	alert ("No ingresaste 15");
+	
+	
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF-03.js
+++ 2-InstruccionIf/jsInstruccionIF-03.js
@@ -1,6 +1,14 @@
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
+	let edad;
 
-}//FIN DE LA FUNCIÓN
+	edad= parseInt(document.getElementById("txtIdEdad").value);
+
+	if(edad >= 18){
+		alert("Sos mayor de edad");
+	}
+	else {
+		alert ("Sos menor de edad"); 
+	}
+
+}
