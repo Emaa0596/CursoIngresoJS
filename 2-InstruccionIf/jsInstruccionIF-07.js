43c29d5e89099ab9ed2f39ff857b7e40fcd46c9e
Emaa0596 <emanueldiaz0596@gmail.com>
2023-02-08 09:18:12 -0300

    Ejercicio 7 y 8

--- 2-InstruccionIf/jsInstruccionIF-07.js
+++ 2-InstruccionIf/jsInstruccionIF-07.js
@@ -1,8 +1,16 @@
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
-	
+	let edad;
+	let estadoCivil;
+	let mensaje;
+
+	edad = parseInt(document.getElementById("txtIdEdad").value);
+	estadoCivil = document.getElementById("estadoCivil").value;
+
+	if(edad < 18 && estadoCivil != "Soltero") {
+		mensaje = "Es muy pequeño para NO ser soltero";
+		alert(mensaje);
+	}
 
 
-}//FIN DE LA FUNCIÓN
+}
