e2483df2eb92a63ad8578c8504caee22ce1d207a
Emaa0596 <emanueldiaz0596@gmail.com>
2023-02-28 00:15:20 -0300

    Parciales

--- 9-Parciales/2020_1Cuatri_parcial_fuentes/uno.js
+++ 9-Parciales/2020_1Cuatri_parcial_fuentes/uno.js
@@ -1,3 +1,144 @@
+/*
+Parcial 1 2020 bis: /"Super chino" Se pide el ingreso de mercadería de un supermercado , 
+hasta que el usuario quiera,se pide :
+tipo(limpieza , comestible , otros)solo estos tres tipos
+nombre del producto
+importe del producto (no mayor a 1000 pesos)
+procedencia(importado, nacional, elaborado)
+Peso (no mayor a 30 kilos)
+
+informar SOLO SI HAY
+a) el NOMBRE del mas pesado de los comestibles
+b) el NOMBRE del mas caro de todos los productos
+c) el NOMBRE del mas barato de los elaborados
+d)el tipo de mercadería que mas aparece
+e)el porcentaje de productos elaborados por sobre el total
+f) el promedio de pesos por cada tipo ingresado
+*/
+function mostrar()
+{
+
+let tipoProducto;
+let importeProducto;
+let procedencia;
+let peso;
+let respuesta = "si";
+let nombreProducto;
+let banderaMasPesado = true;
+let banderaMasCaro = true;
+let banderaMasBarato = true;
+let nombreMasPesado;
+let masPesado = 0;
+let masBarato = 0;
+let masCaro = 0;
+let nombreMasBarato;
+let nombreMasCaro;
+let totalProductos = 0;
+let totalProductosElaborados = 0;
+let tipoMasMercaderia;
+let porcentajeElaborados;
+let totalLimpieza = 0;
+let totalOtros =0;
+let totalComestibles =0;
+let promedioLimpieza;
+let promedioOtros;
+let promedioComestible;
+let cantidadComestibles = 0;
+let cantidadOtros = 0;
+let cantidadLimpieza = 0;
+
+while (respuesta == "si" || respuesta == "Si" || respuesta == "SI"){
+    tipoProducto = prompt("Ingrese el tipo de producto: 'Limpieza','Comestible' o 'Otros'");
+    while (tipoProducto != "Limpieza" && tipoProducto != "Comestible"
+    && tipoProducto != "Otros" && tipoProducto != "limpieza" && tipoProducto != "comestible"
+    && tipoProducto != "otros"){
+      tipoProducto = prompt("Error.Ingrese el tipo de producto: 'limpieza','comestible' o 'otros'");
+    }
+	nombreProducto = prompt("Ingrese el nombre del producto");
+	importeProducto = parseFloat(prompt("Ingrese el importe del producto"));
+	while(isNaN(importeProducto) || importeProducto > 1000 || importeProducto < 0){
+		importeProducto = parseFloat(prompt("Error.Ingrese el importe del producto"));
+	}
+	procedencia = prompt("Ingrese la procedencia: importado,nacional o elaborado.");
+	while(procedencia != "elaborado" && procedencia != "Elaborado" && procedencia != "Nacional" && procedencia != "nacional" && procedencia != "Importado"
+	 && procedencia != "importado"){
+		procedencia = prompt("Error.Ingrese la procedencia: importado,nacional o elaborado.");
+	}
+	peso = parseFloat(prompt("Ingrese el peso del producto"));
+	while(isNaN(peso) || peso < 0 || peso > 30){
+		peso = parseFloat(prompt("Error.Ingrese el peso del producto"));
+	}
+
+	if(banderaMasCaro == true || masCaro < importeProducto){
+		masCaro = importeProducto;
+		nombreMasCaro = nombreProducto;
+		banderaMasCaro = false;
+	}
+	
+	if(procedencia == "elaborado" || procedencia == "Elaborado" || procedencia == "ELABORADO"){
+		totalProductosElaborados = totalProductosElaborados +1;
+		if(banderaMasBarato == true || masBarato < importeProducto){
+			nombreMasBarato = nombreProducto;
+			masBarato = importeProducto
+			banderaMasBarato = false;
+		}
+	}
+	totalProductos = totalProductos + 1;
+	switch(tipoProducto){
+		case "Comestible":
+		case "comestible":
+			cantidadComestibles = cantidadComestibles + 1;
+			totalComestibles = totalComestibles + importeProducto;
+			if(banderaMasPesado == true || masPesado < peso){
+				nombreMasPesado = nombreProducto
+				masPesado = peso;
+				banderaMasPesado = false;
+			}
+			break
+		case "Limpieza":
+		case "limpieza":
+			cantidadLimpieza = cantidadLimpieza +1;
+			totalLimpieza = totalLimpieza + importeProducto;
+			break
+		case "Otros":
+		case "otros":
+			cantidadOtros = cantidadOtros +1;
+			totalOtros = totalOtros + importeProducto;
+			break 
+		}
+	respuesta = prompt("Desea seguir ingresando productos? 'Si' o 'No'?");
+	}// FIN DEL WHILE
+
+	if(cantidadComestibles > cantidadLimpieza && cantidadComestibles > cantidadOtros){
+		tipoMasMercaderia = "Comestibles"
+	}
+	else if(cantidadLimpieza > cantidadComestibles && cantidadLimpieza > cantidadOtros){
+		tipoMasMercaderia = "Limpieza";
+	}
+	else{
+		tipoMasMercaderia = "Otros";
+	}
+	porcentajeElaborados = totalProductosElaborados/ totalProductos*100;
+	promedioComestible = totalComestibles / cantidadComestibles;
+	promedioLimpieza = totalLimpieza/cantidadLimpieza;
+	promedioOtros = totalOtros / cantidadOtros;
+
+	document.write("El nombre del mas pesado de los comestibles es: " + nombreMasPesado +"<br>");
+	document.write("El nombre del mas caro de los productos es: " + nombreMasCaro +"<br>");
+	document.write("El nombre del mas barato de los productos elaborados es: "+nombreMasBarato +"<br>");
+	document.write("El tipo con mas mercaderia es: "+tipoMasMercaderia +"<br>");
+	document.write("El porcentaje de los elaborados por sobre el total es: "+porcentajeElaborados+"%" +"<br>");
+	document.write("El promedio de los comestibles es de: $"+promedioComestible+"<br>");
+	document.write("El promedio de los de limpieza es de: $"+promedioLimpieza +"<br>");
+	document.write("El promedio de otros es de: $"+promedioOtros); 
+
+
+}
+
+
+
+
+
 /*Debemos realizar la carga de 5(cinco) productos de prevención de contagio,
 de cada una debo obtener los siguientes datos:
 el tipo (validar "barbijo" , "jabón" o "alcohol") ,
@@ -9,8 +150,10 @@ a) Del más barato de los alcohol, la cantidad de unidades y el fabricante
 b) Del tipo con mas unidades, el promedio por compra
 c) Cuántas unidades de jabones hay en total 
 */
-function mostrar()
-{
+
+/*
+parcial uno: 
+
 	let tipoProducto;
 	let precioProducto;
 	let marca;
@@ -55,12 +198,6 @@ function mostrar()
 		marca = prompt("Ingrese la marca");
 		fabricante = prompt("Ingrese el fabricante");
 
-		/*Se debe Informar al usuario lo siguiente:
-		a) Del más barato de los alcohol, la cantidad de unidades y el fabricante
-		b) Del tipo con mas unidades, el promedio por compra
-		c) Cuántas unidades de jabones hay en total 
-		*/
-
 		switch(tipoProducto){
 			case "J":
 			case "j":
@@ -104,5 +241,4 @@ function mostrar()
 	document.write("El alcohol mas barato es " + fabricanteBarato + " y se compraron " + masBaratoAlcohol + " unidades" + "<br>" );
 	document.write("el tipo con mas unidades es " + tipoConMasUnidades + " con un promedio de "+ promedioMasUnidades + " unidades por compra"+"<br>" );
 	document.write("La cantidad de jabones en total es: " + contadorJabones);
-
-}
+*/
--- 9-Parciales/2020_1Cuatri_recu_fuentes/dos.js
+++ 9-Parciales/2020_1Cuatri_recu_fuentes/dos.js
@@ -1,4 +1,116 @@
-function mostrar()
-{
-  alert("dos");
+ /*
+Realizar el algoritmo que permita ingresar los datos de una compra de productos de 
+la construccion, hasta que el cliente quiera:
+Tipo validad("arena";"cal";"cemento")
+Cantidad de bolsas(0-300),
+Precio por bolsa (más de cero y menor a 10000 ),
+al terminar la compra el cliente accede a un descuento segun las bolsas en total
+Si compro más de 15 bolsas en total tenes 10% de descuento sobre el total a pagar.
+Si compro más de 45 bolsas en total tenes 30% de descuento sobre el total a pagar.
+a)El importe total a pagar , bruto sin descuento y...
+b)el importe total a pagar con descuento(solo si corresponde)
+d)Informar la bolsa mas cara de Cal y la mas barata de Cemento.
+e)Cuantas bolsas de arena se compraron en total, y el promedio por compra. 
+*/
+function mostrar() {
+  let tipoProducto;
+  let cantidad;
+  let precioPorBolsa;
+  let descuento;
+  let cantidadBolsasTotal;
+  let importeTotalBruto;
+  let importeConDescuento;
+  let banderaCal;
+  let banderaCemento;
+  let bolsaCalMasCara;
+  let bolsaMasBarataCemento;
+  let contadorArena;
+  let contadorBolsasArena;
+  let respuesta;
+  let promedio;
+  let mensajeDescuento;
+
+  descuento = 0;
+  banderaCal = true;
+  banderaCemento = true;
+  contadorArena = 0;
+  respuesta = "si";
+  cantidadBolsasTotal = 0;
+  contadorBolsasArena = 0;
+  importeTotalBruto = 0;
+
+  while (respuesta == "si" || respuesta == "Si" || respuesta == "SI"){
+    tipoProducto = prompt("Ingrese el tipo de producto: 'Cal','Arena' o 'Cemento'");
+    while (tipoProducto != "cal" && tipoProducto != "Cal"
+    && tipoProducto != "Arena" && tipoProducto != "arena" && tipoProducto != "cemento"
+    && tipoProducto != "Cemento"){
+      tipoProducto = prompt("Error.Ingrese el tipo de producto: 'Cal','Arena' o 'Cemento'");
+    }
+    cantidad = parseInt(prompt("Ingrese la cantidad de bolsas"));
+    while(isNaN(cantidad) || cantidad < 0 || cantidad > 300){
+      cantidad = parseInt(prompt("Error.Ingrese la cantidad de bolsas"));
+    }
+    precioPorBolsa = parseFloat(prompt("Ingrese el precio por bolsa"));
+    while(isNaN(precioPorBolsa) || precioPorBolsa < 0 || precioPorBolsa > 10000){
+      precioPorBolsa = parseFloat(prompt("Error.Ingrese el precio por bolsa"));
+    }
+/*al terminar la compra el cliente accede a un descuento segun las bolsas en total
+Si compro más de 15 bolsas en total tenes 10% de descuento sobre el total a pagar.
+Si compro más de 45 bolsas en total tenes 30% de descuento sobre el total a pagar.
+a)El importe total a pagar , bruto sin descuento y...
+b)el importe total a pagar con descuento(solo si corresponde)
+d)Informar la bolsa mas cara de Cal y la mas barata de Cemento.
+e)Cuantas bolsas de arena se compraron en total, y el promedio por compra. 
+*/
+
+    switch(tipoProducto){
+      case "Cal":
+      case "cal":
+        if(banderaCal == true || precioPorBolsa > bolsaCalMasCara){
+          bolsaCalMasCara = precioPorBolsa;
+          banderaCal = false;
+        }
+        break
+      case "Cemento":
+      case "cemento":
+        if(banderaCemento == true || precioPorBolsa < bolsaMasBarataCemento){
+          bolsaMasBarataCemento = precioPorBolsa;
+          banderaCemento = false;
+        }
+        break
+      case "Arena":
+      case "arena":
+          contadorArena = contadorArena + 1;
+          contadorBolsasArena = contadorBolsasArena + cantidad;
+        break
+    }
+
+    cantidadBolsasTotal = cantidadBolsasTotal + cantidad
+    importeTotalBruto = importeTotalBruto + cantidad * precioPorBolsa;
+    respuesta = prompt("Desea seguir ingresando productos? 'Si' o 'No'?");
+  }
+
+  if(cantidadBolsasTotal > 14){
+    descuento = 10;
+    importeConDescuento = importeTotalBruto - importeTotalBruto *(descuento/100);
+    mensajeDescuento = "El importa a pagar con 10% de descuento es: " + importeConDescuento;
+  }
+  else if(cantidadBolsasTotal > 44){
+    descuento = 30;
+    importeConDescuento = importeTotalBruto - importeTotalBruto *(descuento/100);
+    mensajeDescuento = "El importa a pagar con 30% de descuento es: " + importeConDescuento;
+  }
+  else{
+    descuento = 0;
+    mensajeDescuento = "No se aplico ningun descuento";
+  }
+
+  promedio = contadorBolsasArena / contadorArena;
+
+  document.write("El total de la compra bruto sin descuento es de: $" + importeTotalBruto+ "<br>");
+  document.write(mensajeDescuento+ "<br>");
+  document.write("La bolsa mas cara de cal costo: $" +bolsaCalMasCara+ "<br>");
+  document.write("La bolsa mas barata de cemento costo: $"+ bolsaMasBarataCemento+ "<br>");
+  document.write("Se compraron un total de bolsas de arena de: "+contadorBolsasArena + "<br>");
+  document.write("El promedio de de bolsas de arena por compra es de: "+ promedio + " bolsas");
 }
